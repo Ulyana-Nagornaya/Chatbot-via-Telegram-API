f006d00c468df705513fec6d834161f3ba3291e1
Ulyana-Nagornaya <ulyana.nagornaya@gmail.com>
2024-10-19 03:36:41 +0300

    add delete_webhook function

--- main.py
+++ main.py
@@ -53,6 +53,10 @@ class ClubBot:
     def set_webhook(self):
         WEBHOOK_URL = 'https://railway.app/project/86807486-8c6e-4c27-a123-c4aae31b84e5'
         requests.post(f'https://api.telegram.org/bot{token}/setWebhook?url={WEBHOOK_URL}')
+
+    # def delete_webhook(self):
+    #     self.bot.delete_webhook()
+
     def load_data(self):
         data_loader = DataLoader('clear_data.json', 'questions.json')
         data, self.additional_questions = data_loader.load_data()
@@ -120,4 +124,6 @@ class ClubBot:
 
 if __name__ == "__main__":
     club_bot = ClubBot(token)
+    # club_bot.delete_webhook()
+    club_bot.set_webhook()
     # club_bot.start_polling()
